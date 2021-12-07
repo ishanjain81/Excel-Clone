ad2cbb6b59a5224f51788a0f0d2503b81a9b45e4
Ishan Jain <ishanjaincool81@gmail.com>
2021-12-07 12:48:15 +0530

    Cyclic formula detection

---
+++ cycleValidation.js
@@ -0,0 +1,61 @@
+// Storage
+let graphComponentMatrix = [];
+
+for(let i=0;i<rows;i++){
+    let row = [];
+    for(let j=0;j<cols;j++){
+        row.push([]);
+    }
+    graphComponentMatrix.push(row);
+}
+
+// True -> Cycle
+function isGraphCyclic(graphComponentMatrix) {
+    // Dependenvy -> visited, dfsVisited (2D Array)
+    let visited = [];
+    let dfsVisited = [];
+
+    for(let i=0;i<rows;i++){
+        let visitedRow = [];
+        let dfsVisitedRow = [];
+        for(let j=0;j<cols;j++){
+            visitedRow.push(false);
+            dfsVisitedRow.push(false);
+        }
+        visited.push(visitedRow);
+        dfsVisited.push(dfsVisitedRow);
+    }
+
+    for(let i=0;i<rows;i++){
+        for(let j=0;j<cols;j++){
+            if(visited[i][j] === false){
+                let response = dfsCycleDetection(graphComponentMatrix, i, j, visited, dfsVisited);
+                if(response === true){
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+
+function dfsCycleDetection(graphComponentMatrix, srcr, srcc, visited, dfsVisited){
+    visited[srcr][srcc] = true;
+    dfsVisited[srcr][srcc] = true;
+
+    for(let children = 0;children < graphComponentMatrix[srcr][srcc].length;children++){
+        let [crid,ccid] = graphComponentMatrix[srcr][srcc][children];
+        if(visited[crid][ccid] === false){
+            let response = dfsCycleDetection(graphComponentMatrix,crid,ccid,visited,dfsVisited);
+            if(response === true){
+                return true;
+            }
+        }
+        else if(dfsVisited[crid][ccid] === true){
+            return true;
+        }
+    }
+
+    dfsVisited[srcr][srcc] = false;
+    return false;
+}
--- formula.js
+++ formula.js
@@ -29,6 +29,16 @@ formulaBar.addEventListener("keydown",(e) =>{
             removeChildFromParent(cellProp.formula);
         }
 
+        addChildToGraphComponent(inputFormula,address);
+        // Check for cyclic formula
+        let isCyclic = isGraphCyclic(graphComponentMatrix);  // True -> Cycle
+
+        if(isCyclic === true) {
+            alert("Your Formula is Cyclic");
+            removeChildFromGraphComponent(inputFormula, address);
+            return;
+        }
+
         let evaluatedValue = evaluateFormula(inputFormula); 
 
         setCellUIAndCellProp(evaluatedValue, inputFormula, address);
@@ -37,6 +47,32 @@ formulaBar.addEventListener("keydown",(e) =>{
         updateChildrenCells(address);
     }
 });
+
+
+function addChildToGraphComponent(formula, childAddress){
+    let [crid,ccid] = decodeRIDCIDFromAddress(childAddress);
+    let encodedFormula = formula.split(" ");
+    for(let i=0;i<encodedFormula.length;i++){
+        let asciiValue = encodedFormula[i].charCodeAt(0);
+        if(asciiValue >= 65 && asciiValue <= 90){
+            let [prid, pcid] = decodeRIDCIDFromAddress(encodedFormula[i]);
+            graphComponentMatrix[prid][pcid].push([crid,ccid]);
+        }
+    }
+}
+
+function removeChildFromGraphComponent(formula, childAddress){
+    let [crid,ccid] = decodeRIDCIDFromAddress(childAddress);
+    let encodedFormula = formula.split(" ");
+    for(let i=0;i<encodedFormula.length;i++){
+        let asciiValue = encodedFormula[i].charCodeAt(0);
+        if(asciiValue >= 65 && asciiValue <= 90){
+            let [prid, pcid] = decodeRIDCIDFromAddress(encodedFormula[i]);
+            graphComponentMatrix[prid][pcid].pop();
+        }
+    }
+}
+
 
 function updateChildrenCells(parentAddress){
     let [parentCell,parentCellProp] = getCellAndCellProp(parentAddress);
